bffc375194235b12795f59305e872d8239a071cf
MehediHasan16 <maher168082@gmail.com>
2021-08-22 17:49:40 +0600

    delete comment

--- js/app.js
+++ js/app.js
@@ -21,29 +21,18 @@ function optionAmountUpdate(optionInput) {
 
 function updateConfigurationOptionCostTotal() {
     //best price total
-
-    // const defaultBestPrice = document.getElementById("default-cost");
-    // const defaultBestPriceText = defaultBestPrice.innerText;
-    // const defaultBestPriceAmount = parseInt(defaultBestPriceText);
-
     const bestPriceAmount = optionAmountUpdate('default');
 
 
-    //memory cost total
-
-    // const defaultBestPrice = document.getElementById("memory-cost");
-    // const defaultBestPriceText = defaultBestPrice.innerText;
-    // const defaultBestPriceAmount = parseInt(defaultBestPriceText);
+    //memory cost total   
     const memoryPriceAmount = optionAmountUpdate('memory');
 
 
     //storage cost total 
-
     const storagePriceAmount = optionAmountUpdate('storage');
 
 
     //shipping Charge cost total 
-
     const shippingChargeAmount = optionAmountUpdate('delivery');
 
 
@@ -52,14 +41,8 @@ function updateConfigurationOptionCostTotal() {
     // total cost output 
     const totalAmount = document.getElementById('total-cost');
     totalAmount.innerText = selectedImacCostTotal;
-    //apply Pomo code
-    /* document.getElementById("apply-btn").addEventListener('click', function () {
-        console.log(' pomo code click')
-        const applyPomoCode = document.getElementById("input-pomo-code");
-        const applyPomoCodeText = applyPomoCode.value;
-        console.log(applyPomoCodeText);
-    });
- */
+
+
     // total lest  cost output 
     const totalLestAmount = document.getElementById("totalLest-cost")
     totalLestAmount.innerText = selectedImacCostTotal;
@@ -101,48 +84,32 @@ document.getElementById("memory-8GB").addEventListener("click", function () {
 
 document.getElementById("memory-16GB").addEventListener("click", function () {
 
-    // const memoryCost = document.getElementById("memory-cost");
-    // const memoryCostText = memoryCost.innerText;
-    // memoryCost.innerText = 180;
+
     configurationOptionCostUpdate('memory', 180);
 });
 
 // storage part 
 document.getElementById("storage-256SSD").addEventListener("click", function () {
 
-    // const storageSsd = document.getElementById("storage-cost");
-    // const memoryCostText = storageSsd.innerText;
-    // storageSsd.innerText = 0;
+
     configurationOptionCostUpdate('storage', 0);
 });
 
 
 document.getElementById("storage-512SSD").addEventListener("click", function () {
 
-    // const storageSsd = document.getElementById("storage-cost");
-    // const memoryCostText = storageSsd.innerText;
-    // storageSsd.innerText = 100;
     configurationOptionCostUpdate('storage', 100);
 
 });
 document.getElementById("storage-1tbSSD").addEventListener("click", function () {
 
-    // const storageSsd = document.getElementById("storage-cost");
-    // const memoryCostText = storageSsd.innerText;
-    // storageSsd.innerText = 180;
-
     configurationOptionCostUpdate('storage', 180);
 });
-
-
 
 //delivery charge
 
 document.getElementById("delivery-free").addEventListener("click", function () {
 
-    // const deliveryCost = document.getElementById("delivery-cost");
-    // const deliveryCostText = deliveryCost.innerText;
-    // deliveryCost.innerText = 0;
     configurationOptionCostUpdate('delivery', 0);
 
 });
@@ -150,9 +117,6 @@ document.getElementById("delivery-free").addEventListener("click", function () {
 
 document.getElementById("delivery-charge").addEventListener("click", function () {
 
-    // const deliveryCost = document.getElementById("delivery-cost");
-    // const deliveryCostText = deliveryCost.innerText;
-    // deliveryCost.innerText = 20;
     configurationOptionCostUpdate('delivery', 20);
 });
 
